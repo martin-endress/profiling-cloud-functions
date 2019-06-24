1ebe5a617794c2ee878ab1de8d8f23aa3bd4df30
Martin Endress <martin.endress@stud.uni-bamberg.de>
2019-06-24 14:24:39 +0200

    cpumetrics

--- src/de/uniba/dsg/serverless/profiling/model/CPUMetrics.java
+++ src/de/uniba/dsg/serverless/profiling/model/CPUMetrics.java
@@ -2,18 +2,19 @@ package de.uniba.dsg.serverless.profiling.model;
 
 import de.uniba.dsg.serverless.profiling.util.MetricsUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class CPUMetrics extends Metrics {
+public class CPUMetrics extends Metrics<CPUMetrics> {
 
-    private final String time;
-    private final Map<String, Long> metrics;
+    private final long time;
 
-    public CPUMetrics(List<String> lines, String time) throws ProfilingException {
+    public CPUMetrics(List<String> lines, long time) throws ProfilingException {
+        RELEVANT_METRICS = Arrays.asList("user", "system");
+        metrics = util.fromLines(lines);
+        validate();
         this.time = time;
-        metrics = new MetricsUtil().fromLines(lines);
     }
-
 
 }
