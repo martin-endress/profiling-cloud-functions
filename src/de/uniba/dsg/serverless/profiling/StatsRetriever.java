051e9aa8e11712b0eb7681abcf67f5aa080707dc
Martin Endress <martin.endress@stud.uni-bamberg.de>
2019-06-24 14:24:12 +0200

    systemcall test

--- src/de/uniba/dsg/serverless/profiling/StatsRetriever.java
+++ src/de/uniba/dsg/serverless/profiling/StatsRetriever.java
@@ -4,13 +4,12 @@ import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.model.Statistics;
 import com.google.common.util.concurrent.Uninterruptibles;
 import de.uniba.dsg.serverless.profiling.docker.ContainerProfiling;
-import de.uniba.dsg.serverless.profiling.model.MemoryMetrics;
-import de.uniba.dsg.serverless.profiling.model.Metrics;
-import de.uniba.dsg.serverless.profiling.model.Profile;
-import de.uniba.dsg.serverless.profiling.model.ProfilingException;
+import de.uniba.dsg.serverless.profiling.model.*;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -35,7 +34,7 @@ public class StatsRetriever {
 
     public static void main(String[] args) {
         try {
-            new StatsRetriever().retrieveStats();
+            new StatsRetriever().testSystemCall();
         } catch (ProfilingException e) {
             System.err.println(e.getMessage());
             System.err.println(Optional
@@ -46,6 +45,24 @@ public class StatsRetriever {
         }
     }
 
+    public void testSystemCall() throws ProfilingException {
+        try {
+            Process p = Runtime.getRuntime().exec("ls");
+            try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                 BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                String s;
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                }
+                while ((s = stdError.readLine()) != null) {
+                    System.out.println(s);
+                }
+            }
+        } catch (IOException e) {
+            throw new ProfilingException(e);
+        }
+    }
+
     public void retrieveStats() throws ProfilingException {
         long startTime = System.currentTimeMillis();
         ContainerProfiling profiling = new ContainerProfiling();
@@ -53,14 +70,18 @@ public class StatsRetriever {
         System.out.println("Container (id=" + containerId + ") started.");
 
         Path memoryLogs = getMemoryLogsPath(containerId);
-        List<Metrics> metrics = new ArrayList<>();
-        while (metricsAvailable(memoryLogs)) {
-            metrics.add(getMemoryMetrics(memoryLogs));
+        Path cpuLogs = getCPULogsPath(containerId);
+        List<MemoryMetrics> memoryMetrics = new ArrayList<>();
+        List<CPUMetrics> cpuMetrics = new ArrayList<>();
+        while (metricsAvailable(memoryLogs) && metricsAvailable(cpuLogs)) {
+            //FIXME fix me, not safe
+            memoryMetrics.add(getMemoryMetrics(memoryLogs));
+            cpuMetrics.add(getCPUMetrics(memoryLogs));
             Uninterruptibles.sleepUninterruptibly(100, TimeUnit.MILLISECONDS);
         }
 
         InspectContainerResponse additional = profiling.inspectContainer();
-        Profile p = new Profile(metrics, additional);
+        Profile p = new Profile(cpuMetrics, additional);
 
         //List<Statistics> stats = profiling.logStatistics();
         //List<MemoryMetrics> memoryMetrics = new ArrayList<>();
@@ -78,6 +99,16 @@ public class StatsRetriever {
         try {
             List<String> lines = Files.readAllLines(path);
             return new MemoryMetrics(lines, currentTime);
+        } catch (IOException e) {
+            throw new ProfilingException("Could not read file " + path + ".", e);
+        }
+    }
+
+    private CPUMetrics getCPUMetrics(Path path) throws ProfilingException {
+        long currentTime = System.currentTimeMillis() - startTime;
+        try {
+            List<String> lines = Files.readAllLines(path);
+            return new CPUMetrics(lines, currentTime);
         } catch (IOException e) {
             throw new ProfilingException("Could not read file " + path + ".", e);
         }
